1e40e23f271a3b3b2ac4b9b9018314cbe04ccccd
anto991nio <antonio.91.cardone@gmail.com>
2021-06-21 09:36:38 +0200

    Termina pagina default

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,41 @@
 @extends('default')
+
+@section('contenuto-centrale')
+
+<div class="card-container">
+    <div class="container d-flex">
+        <div class="current">
+        <h3>CURRENT SERIES</h3>
+        </div>
+
+        @foreach($comicsList as $comic)
+
+        <div class="card">
+            <div class="card-img">
+            <a href="#">
+            <img src="{{ $comic['thumb'] }}" alt="">
+            </a>
+            </div>
+            <a href="#" class="titolo">
+            <p class="titolo"><strong > {{ $comic['title'] }}</strong></p>
+            </a>
+        </div>
+
+
+        @endforeach
+
+        <h5 class="load">LOAD MORE</h5>
+    </div>
+    
+</div>
+<div class="mid-bar">
+<div class="container d-flex">
+<div class="d-flex card-mid"><a href="#" class="d-flex"><img src="{{asset('img\buy-comics-digital-comics.png')}}" alt=""><p>DIGITAL COMICS</p></a></div>
+<div class="d-flex card-mid"><a href="#" class="d-flex"><img src="{{asset('img\buy-comics-merchandise.png')}}" alt=""><p>DC MERCHANDISE</p></a></div>
+<div class="d-flex card-mid"><a href="#" class="d-flex"><img src="{{asset('img\buy-comics-subscriptions.png')}}" alt=""><p>SUBSCRIPTION</p></a></div>
+<div class="d-flex card-mid"><a href="#" class="d-flex"><img src="{{asset('img\buy-comics-shop-locator.png')}}" alt=""><p>COMIC SHOP LOCATOR</p></a></div>
+<div class="d-flex card-mid"><a href="#" class="d-flex"><img src="{{asset('img\buy-dc-power-visa.svg')}}" alt=""><p>DC POWER VISA</p></a></div>
+</div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-    
+    $datiComics = config("comics");
+
+    $datiView = [
+        "comicsList" => $datiComics    ];
+
+    return view("home", $datiView );  
 });
